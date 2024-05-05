9fa091b5dc1b6ae19d8bab12f8f38983f8b144c5
Vladimir Drugov <wolffdrugov@yandex.ru>
2024-05-05 18:26:28 +0300

    yes

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,12 @@
 package main
 import (
     "html/template"
-    "io"
-    "net/mail"
+    "io--primary: #005b99;
+--important:#ff7f00; 
+--bg:#f7f7f7;
+--border:#d3d3d3; 
+--stand-out:#ffcc00;
+"
     "time"
 
     "github.com/labstack/echo/v4"
@@ -13,7 +17,7 @@ type Template struct {
     tmpls *template.Template
 }
 
-func (t *Template) Render (w io.Writer, name string, data interface{}, c echo.Context) error {
+func (t *Template) Render (w io.Writer, name string, data interface{}, c echo.Context) error  {
     return t.tmpls.ExecuteTemplate( w, name , data )
 }
 

dc5f173c2149bee5c3c79c560bbc1e6dac30cc7f
Vladimir Drugov <wolffdrugov@yandex.ru>
2024-05-10 05:49:42 +0300

    input groups done

--- static/js/range.js
+++ static/js/range.js
@@ -1,16 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const rangeEl = document.querySelector(".js-calculator__range-input");
-    if (!rangeEl) return;
-    const rangeLabelEl = document.querySelector(".js-range-input__label");
-    if (!rangeLabelEl) return;
-    const ratio = rangeLabelEl.offsetWidth / rangeEl.offsetWidth;
-
-    rangeLabelEl.dataset.ratio = ratio.toFixed(2);
-
-    rangeEl.addEventListener("input", function() {
-        rangeLabelEl.textContent = rangeEl.value;
-        const translate = calcTooltipTranslate(rangeEl, rangeLabelEl);
-    });
+    initSliders();
 });
 
 function calcTooltipTranslate(rangeEl, rangeLabelEl) {
@@ -19,10 +8,54 @@ function calcTooltipTranslate(rangeEl, rangeLabelEl) {
 
     translate =
         ((+(+rangeEl.value) - +rangeEl.min) / valueDiff) *
-            (parseFloat(rangeEl.offsetWidth) -
-                parseFloat(rangeLabelEl.offsetWidth) / 2) - 15;
+        (parseFloat(rangeEl.offsetWidth) -
+            parseFloat(rangeLabelEl.offsetWidth) / 2) -
+        15;
 
     //this is kinda terrible tbh
     const resultString = `translateX(${translate}px)`;
+
+    if (rangeEl.dataset.type == "money") {
+        const newValue = formatValueToMoney(rangeEl.value, +rangeEl.max);
+        console.log(newValue, " new value");
+        rangeLabelEl.textContent = newValue;
+    } else {
+        rangeLabelEl.textContent = rangeEl.value;
+    }
     rangeLabelEl.style.transform = resultString;
+}
+
+function initSliders() {
+    const rangeEl = document.querySelectorAll(".js-calculator__range-input");
+    if (!rangeEl) return;
+    const rangeLabelElAll = document.querySelectorAll(".js-range-input__label");
+    if (!rangeLabelElAll) return;
+    rangeEl.forEach((rangeEl, index) => {
+        const rangeLabelEl = rangeLabelElAll[index];
+        calcTooltipTranslate(rangeEl, rangeLabelEl);
+        const ratio = rangeLabelEl.offsetWidth / rangeEl.offsetWidth;
+
+        rangeLabelEl.dataset.ratio = ratio.toFixed(2);
+
+        rangeEl.addEventListener("input", function() {
+            const translate = calcTooltipTranslate(rangeEl, rangeLabelEl);
+        });
+    });
+}
+
+function formatValueToMoney(value, max) {
+    const money = value;
+    const formatter = new Intl.NumberFormat("ru-ru", {
+        notation: "compact",
+        minimumFractionDigits: 0,
+        maximumSignificantDigits: 3,
+        style: "currency",
+        currency: "RUB",
+    });
+
+    let newValue = formatter.format(money);
+    if (value == max) {
+        newValue = '>' + newValue;
+    }
+    return newValue;
 }

0398062b66bd850ae55e5ef82bee22eace0ceea6
Vladimir Drugov <wolffdrugov@yandex.ru>
2024-05-29 20:51:02 +0300

    master:still hating frontend

--- static/slider_range2/slider_range2.js
+++ static/slider_range2/slider_range2.js
@@ -1,5 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
 	const slider = document.querySelector('#js-slider-range-age');
+	console.log(slider,' slider')
+	const minHiddenInput = document.querySelector("[name='age-min']")
+	const maxHiddenInput = document.querySelector("[name='age-max']")
+	if(!minHiddenInput || !maxHiddenInput) {
+		return;
+	}
+	//TODO add debouncer
 	slider.addEventListener('change', (evt) => {
+		minHiddenInput.value = evt.detail.value1;
+		maxHiddenInput.value = evt.detail.value2;
 	});
 })
